f2a8ae7597713bcf9387249aa1d45f3969b054af
William <w.morin.laberge@gmail.com>
2020-10-17 16:58:23 -0400

    Basis for editing simulation parameters

--- app/src/main/java/com/concordia/smarthomesimulator/fragments/dashboard/DashboardController.java
+++ app/src/main/java/com/concordia/smarthomesimulator/fragments/dashboard/DashboardController.java
@@ -1,32 +1,50 @@
 package com.concordia.smarthomesimulator.fragments.dashboard;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import com.concordia.smarthomesimulator.R;
+import com.concordia.smarthomesimulator.activities.editDashboard.EditDashboardController;
+import com.concordia.smarthomesimulator.activities.login.LoginController;
+import com.concordia.smarthomesimulator.activities.main.MainController;
+import com.concordia.smarthomesimulator.dataModels.LogEntry;
+import com.concordia.smarthomesimulator.dataModels.LogImportance;
+import com.concordia.smarthomesimulator.helpers.ActivityLogHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DashboardController extends Fragment {
 
     private DashboardModel dashboardModel;
+    private View view;
+    private Context context;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         dashboardModel = new ViewModelProvider(this).get(DashboardModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        dashboardModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        context = getActivity();
+
+        setupEditIntent();
+
+        return view;
+    }
+
+    private void setupEditIntent() {
+        FloatingActionButton fab = view.findViewById(R.id.fab_edit_dashboard);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                //proceeding to the next activity and logging what happened
+                Intent intent = new Intent(context, EditDashboardController.class);
+                ActivityLogHelper.add(context, new LogEntry("Dashboard","Starting EditDashboard", LogImportance.MINOR));
+                context.startActivity(intent);
             }
         });
-        return root;
     }
+
 }
