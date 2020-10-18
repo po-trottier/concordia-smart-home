ec47029fd5721e7699563b2e72da090c8e272142
qandeelarshad <qandeelsphoto@gmail.com>
2020-10-18 01:59:45 -0400

    dashboard model and controller modified

--- app/src/main/java/com/concordia/smarthomesimulator/fragments/dashboard/DashboardModel.java
+++ app/src/main/java/com/concordia/smarthomesimulator/fragments/dashboard/DashboardModel.java
@@ -1,12 +1,12 @@
 package com.concordia.smarthomesimulator.fragments.dashboard;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
+import android.media.Image;
+
 import androidx.lifecycle.ViewModel;
 
 public class DashboardModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+ /*   private MutableLiveData<String> mText;
 
     public DashboardModel() {
         mText = new MutableLiveData<>();
@@ -15,5 +15,56 @@ public class DashboardModel extends ViewModel {
 
     public LiveData<String> getText() {
         return mText;
+    } */
+
+    public String sw = "On/Off";
+    public String btn = "Edit Simulation";
+    public String user = "Parent";
+    public String location = "Location";
+    public String room = "Kitchen";
+    public String temperature = "Outside temperature: 15°C";
+    public String date = "Friday 16th October 2020";
+    public String time = "10:38:20";
+
+    public DashboardModel() {
+
     }
+
+    public String getSw(){
+        return sw;
+    }
+
+    public String getBtn(){
+        return btn;
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public String getRoom(){
+        return room;
+    }
+
+    public String getTemperature(){
+        return temperature;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
 }
+
+
+
+
+
