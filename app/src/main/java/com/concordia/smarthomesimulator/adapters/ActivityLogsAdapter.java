5e50f1f03b0d7d2d16164d681439681829bc1173
Luigi Besani Urena <luigi.besani@gmail.com>
2020-10-18 23:15:34 -0400

    PR fixes n.2

--- app/src/main/java/com/concordia/smarthomesimulator/adapters/ActivityLogsAdapter.java
+++ app/src/main/java/com/concordia/smarthomesimulator/adapters/ActivityLogsAdapter.java
@@ -15,35 +15,53 @@ import java.util.ArrayList;
  * This code is based off/inspired by a tutorial named "ListView custom adapter"
  * by VoidRealms on Youtube
  * URL : https://www.youtube.com/watch?v=1olQnH9bE2c
- * */
-
+ *
+ * ActivityLogAdapter serves as bridge between our data (activity logs) and the ListView within
+ * the fragments_logs.xml file. From here the format of the text
+ * and the color of the text to be added are determined.
+ */
 public class ActivityLogsAdapter extends ArrayAdapter<ArrayList<LogEntry>> {
 
     ArrayList<LogEntry> items;
     Context ctx;
 
     /**
-     * ActivityLogAdapter serves as bridge between our data (activity logs) and the ListView within
-     * the fragments_logs.xml file. From here the format of the text
-     * and the color of the text to be added are determined.
-     * */
-
+     * Constructor for the ActivityLogAdapter
+     *
+     * @param context  application environment
+     * @param resource
+     * @param list list containing activity log entries to be inserted into items
+     */
     public ActivityLogsAdapter(@NonNull Context context, int resource, ArrayList<LogEntry> list) {
         super(context, resource);
         items = list;
         ctx = context;
     }
 
+    /**
+     * @return number of logs stored
+     */
     @Override
     public int getCount(){
         return items.size();
     }
 
+    /**
+     * This method allows for the inflation of the textView to be inserted
+     * within the fragments_log ListView as an item.
+     * Formatting of the text is also applied here.
+     *
+     * @param position index used for item position
+     * @param convertView
+     * @param parent
+     * @return a list item
+     */
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         LayoutInflater inflater = LayoutInflater.from(ctx);
         View row = inflater.inflate(R.layout.activity_log_list_item, parent, false);
+
         TextView entryView = (TextView)row.findViewById(R.id.entry);
         LogEntry entry = items.get(position);
         entryView.setText(entry.getComponent().toString() + ": " + entry.getMessage().toString() + " " + entry.getDateTime().toString() + " " + entry.getImportance().toString());
@@ -53,13 +71,12 @@ public class ActivityLogsAdapter extends ArrayAdapter<ArrayList<LogEntry>> {
                 entryView.setTextColor(Color.RED);
                 break;
             case IMPORTANT:
-                entryView.setTextColor(Color.BLACK);
+                entryView.setTextColor(Color.rgb(228,123,0));
                 break;
             default:
-                entryView.setTextColor(Color.GRAY);
+                entryView.setTextColor(Color.BLACK);
                 break;
         }
         return row;
     }
-
 }
--- app/src/main/java/com/concordia/smarthomesimulator/fragments/logs/LogsController.java
+++ app/src/main/java/com/concordia/smarthomesimulator/fragments/logs/LogsController.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -15,30 +13,35 @@ import com.concordia.smarthomesimulator.R;
 import com.concordia.smarthomesimulator.adapters.ActivityLogsAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+
 public class LogsController extends Fragment {
 
     private Context context;
     private LogsModel logsModel;
-    private ListView logList;
 
+    /**
+     * This method is created containing the activity logs and a button used to clear all logs
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return View containing the activity logs and a button used to clear all logs.
+     */
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         context = getActivity();
         logsModel = new ViewModelProvider(this).get(LogsModel.class);
         logsModel.readLogs(context);
 
         View root = inflater.inflate(R.layout.fragment_logs, container, false);
-        logList = (ListView)root.findViewById(R.id.activity_logs_list);
-        ActivityLogsAdapter adapter = new ActivityLogsAdapter(getContext(),0,logsModel.getLogs());
+        ListView logList = root.findViewById(R.id.activity_logs_list);
+        ActivityLogsAdapter adapter = new ActivityLogsAdapter(context,0,logsModel.getLogs());
         logList.setAdapter(adapter);
-        final TextView logsCount = root.findViewById(R.id.logs_count_text);
-        logsCount.setText(logsModel.getLogs().size() + " log(s) found");
-        final FloatingActionButton clearLogs = root.findViewById(R.id.clear_logs_button);
 
+        final FloatingActionButton clearLogs = root.findViewById(R.id.clear_logs_button);
         clearLogs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 logsModel.clearLogs(context);
-                logsCount.setText(logsModel.getLogs().size() + " log(s) found");
                 adapter.notifyDataSetChanged();
             }
         });
