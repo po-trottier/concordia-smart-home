bcae0a7a1078af12ee09e4ebb951b6906abf4258
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-11-08 04:36:14 -0500

    Added Authority Notification

--- app/src/main/java/com/concordia/smarthomesimulator/helpers/NotificationsHelper.java
+++ app/src/main/java/com/concordia/smarthomesimulator/helpers/NotificationsHelper.java
@@ -8,11 +8,17 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import com.concordia.smarthomesimulator.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static androidx.core.content.ContextCompat.getSystemService;
 import static com.concordia.smarthomesimulator.Constants.*;
 
 public class NotificationsHelper {
 
+    private static final int MINUTES_TO_MILLISECONDS = 60000;
+
+    private static Timer timer;
     private static int id = 0;
 
     /**
@@ -38,15 +44,41 @@ public class NotificationsHelper {
      * @param context the context
      */
     public static void sendIntruderNotification(Context context){
+        // Get the delay from preferences
         SharedPreferences preferences = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
         int delay = preferences.getInt(PREFERENCES_KEY_CALL_DELAY, DEFAULT_CALL_DELAY);
-
+        float scale = preferences.getFloat(PREFERENCES_KEY_TIME_SCALE, DEFAULT_TIME_SCALE);
+        // Set timer to send authorities notification
+        if (timer != null) {
+            timer.cancel();
+            timer.purge();
+        }
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                sendAuthoritiesNotification(context);
+            }
+        }, (long) ((delay * MINUTES_TO_MILLISECONDS) / scale));
+        // Build notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL);
         builder.setContentTitle(context.getString(R.string.intruder_title));
         builder.setContentText(String.format(context.getString(R.string.intruder_text), delay));
         builder.setSmallIcon(R.drawable.ic_home);
         builder.setAutoCancel(false);
+        // Send notification
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        managerCompat.notify(id++, builder.build());
+    }
 
+    public static void sendAuthoritiesNotification(Context context){
+        // Build notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL);
+        builder.setContentTitle(context.getString(R.string.authorities_title));
+        builder.setContentText(context.getString(R.string.authorities_text));
+        builder.setSmallIcon(R.drawable.ic_home);
+        builder.setAutoCancel(false);
+        // Send notification
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
         managerCompat.notify(id++, builder.build());
     }
